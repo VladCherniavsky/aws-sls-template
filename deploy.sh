2d67fdbf523c0bdaac918b7c0ea966b3637da9d4
Uladzislau Charniauski <vlad.cherniavsky@gmail.com>
2022-07-11 13:52:05 +0300

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ echo '$env:                ' "$env"
 echo '$CODEBUILD_SRC_DIR:  ' "$CODEBUILD_SRC_DIR"
 
 npm install -g serverless
+sls -v
 serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/$env -v -r eu-central-1
