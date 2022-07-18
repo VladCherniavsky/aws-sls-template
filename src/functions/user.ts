23976a11fd08bfe4a0f986ff4e34b7098295771b
Uladzislau Charniauski <vlad.cherniavsky@gmail.com>
2022-07-19 01:49:13 +0300

    Write to dynamodb

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,13 +1,31 @@
+import { DynamoDB } from 'aws-sdk';
 
+const ddb = new DynamoDB.DocumentClient();
 
 export const addUser = async(event: any) => {
-    return {
-        statusCode: 200,
-        message: 'hello',
-        body: {
-            message: 'Hi Vladislav',
-            input: event,
-            env: process.env
-        },
+    try {
+        const params = {
+            Item: {
+                email: 'test@gmail.com',
+                name: 'Vlad'
+            },
+            TableName: process.env.TABLE_NAME!
+        }
+        const res = await ddb.put(params).promise()
+        
+        return {
+            statusCode: 200,
+            message: 'hello',
+            body: {
+                message: 'Hi Vladislav',
+                input: event,
+                res
+            },
+        }
+    } catch(error) {
+        return {
+            error: error
+        }
     }
+
 }
