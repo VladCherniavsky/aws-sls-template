7b5a846cbdd40f18dd8bb023f383ac38f77cafc9
Uladzislau Charniauski <vlad.cherniavsky@gmail.com>
2022-07-19 01:18:59 +0300

    Remove strigify from lambda

--- src/functions/user.ts
+++ src/functions/user.ts
@@ -4,13 +4,9 @@ export const addUser = async(event: any) => {
     return {
         statusCode: 200,
         message: 'hello',
-        body: JSON.stringify(
-            {
-                message: 'Hi Vladislav',
-                input: event,
-            },
-            null,
-            2
-        ),
+        body: {
+            message: 'Hi Vladislav',
+            input: event,
+        },
     }
 }
