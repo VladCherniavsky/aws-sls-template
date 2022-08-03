51286d753a854b55b0e1b8ddcc1faac1e955f92f
Uladzislau Charniauski <vlad.cherniavsky@gmail.com>
2022-08-04 01:10:26 +0300

    Add sqs

--- src/functions/handler.ts
+++ src/functions/handler.ts
@@ -12,7 +12,4 @@ export const hello = async (event) => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
--- src/functions/user.ts
+++ src/functions/user.ts
@@ -1,18 +1,20 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB, SNS } from 'aws-sdk';
+import { SNSEvent, DynamoDBStreamEvent } from 'aws-lambda';
 
 const ddb = new DynamoDB.DocumentClient();
+const sns = new SNS();
 
 export const addUser = async(event: any) => {
     try {
         const params = {
             Item: {
-                email: 'test@gmail.com',
-                name: 'Vlad'
+                email: event.body.email,
+                name: event.body.name
             },
             TableName: process.env.TABLE_NAME!
         }
         const res = await ddb.put(params).promise()
-        
+
         return {
             statusCode: 200,
             message: 'hello',
@@ -28,4 +30,46 @@ export const addUser = async(event: any) => {
         }
     }
 
+}
+
+export const handleUserTableUpdate = async (event: DynamoDBStreamEvent) => {
+    try {
+        console.log('event', event)
+        console.log('process.env.USER_TOPIC_ARN', process.env.USER_TOPIC_ARN)
+        console.log('process.env', process.env)
+        const record = event.Records[0]
+
+        console.log('record.dynamodb', record.dynamodb)
+        const params = {
+            TopicArn: process.env.USER_TOPIC_ARN,
+            Message: JSON.stringify({
+                message: 'user added',
+                data: record.dynamodb
+            })
+        }
+        await sns.publish(params).promise()
+
+        return {
+            event
+        }
+    } catch(error) {
+        console.log('error', error)
+        return {
+            error: error
+        }
+    }
+}
+
+export const handleSnsUserTopicEvent = async (event: SNSEvent) => {
+    try {
+        console.log('handleSnsUserTopicEvent event', event)
+        console.log('handleSnsUserTopicEvent event', event.Records[0].Sns)
+        return {
+            event
+        }
+    } catch(error) {
+        return {
+            error: error
+        }
+    }
 }
